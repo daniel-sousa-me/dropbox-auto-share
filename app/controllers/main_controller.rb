c974a8b45844e8e3c651bbca362afa1579b1b1a5
Daniel Sousa <daniel@sousa.me>
2013-09-20 19:46:25 +0100

    Now the script runs as a delayed job (yay!)

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require "headless"
-require "selenium-webdriver"
-
 class MainController < ApplicationController
   # TODO: Store the folders somewhere (not in the code) and have a script to automatically update the list
   @@folders = [
@@ -151,7 +148,6 @@ class MainController < ApplicationController
     @folders = @@folders
   end
   
-  # TODO: Make this a delayed job
   def submit
     folders_s = []
     @@folders.each_index do |i|
@@ -170,54 +166,7 @@ class MainController < ApplicationController
       end
     end
     
-    attempts = 0
-    begin
-      browser_stuff folders_s, params[:post][:email]
-    rescue
-      attempts += 1
-      retry if attempts<3
-    else
-      attempts = 73
-    end
-    
-    browser_stuff folders_s, params[:post][:email] if attempts!=73 
+    MainHelper.delay.browser_stuff folders_s, params[:post][:email]
   end
   
-  def browser_stuff folders_s, email
-    # TODO: Probably there's a better way of doing this
-    app_config = YAML.load_file("#{Rails.root}/config/config.yml")
-    
-    headless = Headless.new
-    headless.start
-    
-    driver = Selenium::WebDriver.for :firefox
-    begin
-      driver.manage.timeouts.implicit_wait = 10
-      driver.navigate.to "http://dropbox.com/login"
-    
-      driver.execute_script("document.getElementById('login_email').value = '#{app_config["DROPBOX_EMAIL"]}'")
-      driver.execute_script("document.getElementById('login_password').value = '#{app_config["DROPBOX_PASSWORD"]}'")
-      element_signin = driver.find_element(:id, 'login_submit')
-      element_signin.click
-    
-      folders_s.each do |folder|
-        folder[:subfolders].each do |subfolder|
-          driver.navigate.to "http://dropbox.com/home/LMAC/#{folder[:name]}/#{subfolder[:name]}"
-          share_button = driver.find_element(:id, 'global_share_button')
-          share_button.click
-
-          email_input = driver.find_element(:id, 'sharing-options-new-collab-input')
-          email_input.click
-          driver.execute_script("document.getElementById('sharing-options-new-collab-input').value = '#{email}'")
-          send_button = driver.find_element(:id, 'share-invite-button')
-          send_button.click
-        end
-      end
-    
-      driver.navigate.to "http://dropbox.com/logout"
-    ensure
-      driver.quit
-    end
-    headless.destroy
-  end
 end
--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,56 @@
+require "headless"
+require "selenium-webdriver"
+
 module MainHelper
+  def self.browser_stuff folders_s, email
+    attempts = 0
+    begin
+      browser_stuff_helper folders_s, email
+    rescue
+      attempts += 1
+      retry if attempts<3
+    else
+      attempts = 73
+    end
+    
+    browser_stuff_helper folders_s, email if attempts!=73
+  end
+  
+  def self.browser_stuff_helper folders_s, email
+    # TODO: Probably there's a better way of doing this
+    app_config = YAML.load_file("#{Rails.root}/config/config.yml")
+    
+    headless = Headless.new
+    headless.start
+    
+    driver = Selenium::WebDriver.for :firefox
+    begin
+      driver.manage.timeouts.implicit_wait = 10
+      driver.navigate.to "http://dropbox.com/login"
+    
+      driver.execute_script("document.getElementById('login_email').value = '#{app_config["DROPBOX_EMAIL"]}'")
+      driver.execute_script("document.getElementById('login_password').value = '#{app_config["DROPBOX_PASSWORD"]}'")
+      element_signin = driver.find_element(:id, 'login_submit')
+      element_signin.click
+    
+      folders_s.each do |folder|
+        folder[:subfolders].each do |subfolder|
+          driver.navigate.to "http://dropbox.com/home/LMAC/#{folder[:name]}/#{subfolder[:name]}"
+          share_button = driver.find_element(:id, 'global_share_button')
+          share_button.click
+
+          email_input = driver.find_element(:id, 'sharing-options-new-collab-input')
+          email_input.click
+          driver.execute_script("document.getElementById('sharing-options-new-collab-input').value = '#{email}'")
+          send_button = driver.find_element(:id, 'share-invite-button')
+          send_button.click
+        end
+      end
+    
+      driver.navigate.to "http://dropbox.com/logout"
+    ensure
+      driver.quit
+    end
+    headless.destroy
+  end
 end
